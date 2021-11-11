ae5e38a9c19b6b7bcba83d0aca54b7a7fedef138
pinkphi <pamela.u.a@gmail.com>
2021-11-10 21:44:54 -0500

    Added footer to link to github and portfolio

--- src/App.js
+++ src/App.js
@@ -4,7 +4,21 @@ import Weather from'./Weather';
 function App() {
   return (
     <div className="App">
+      <h1>Weather App</h1>
       <Weather />
+      <footer>
+        This project was coded by <a href="https://portafolio-pamela.netlify.app" 
+        target="_blank"
+        rel="noreferrer"
+        >
+          PinkPhi</a> - {" "}
+        <a href="https://github.com/pinkphi/react-weather-2.0"
+        target="_blank"
+        rel="noreferrer"
+        >
+          open-sourced on Github 
+        </a>
+      </footer>
     </div>
   );
 }
