f9a618110b6e26081b6e4fe21de8830be14e28dc
pinkphi <pamela.u.a@gmail.com>
2021-11-30 22:48:27 -0500

    fixed bug

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,52 +1,45 @@
 import React, { useState, useEffect } from "react";
-import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
 import axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay";
 
 export default function WeatherForecast(props) {
-  
-    const [loaded, setLoaded] = useState(false);
-    const [forecast, setForecast] = useState(null);
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
 
-    useEffect(() => {
-      setLoaded(false);
-    }, [props.coordinates]);
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
 
-    function handleResponse(response) {
-        setForecast(response.data.daily);
-        setLoaded(true);
-      }
-    
-    if (loaded) {
-        return (
-          <div className="WeatherForecast">
-            <div className="row">
-              <div className="col">
-                <WeatherForecastDay data={forecast[0]} />
-              </div>
-              <div className="col">
-                <WeatherForecastDay data={forecast[1]} />
-              </div><div className="col">
-                <WeatherForecastDay data={forecast[2]} />
-              </div><div className="col">
-                <WeatherForecastDay data={forecast[3]} />
-              </div><div className="col">
-                <WeatherForecastDay data={forecast[4]} />
-              </div>
-            </div>
-          </div>
-        );
+  function handleResponse(response) {
+    setForecast(response.data.daily);
+    setLoaded(true);
+  }
 
-} else { 
-
+  if (loaded) {
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          {forecast.map(function (dailyForecast, index) {
+            if (index < 5) {
+              return (
+                <div className="col" key={index}>
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            }
+          })}
+        </div>
+      </div>
+    );
+  } else {
     let apiKey = "9450c4e093311119f00946463ff53dcd";
     let longitude = props.coordinates.lon;
     let latitude = props.coordinates.lat;
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
 
     axios.get(apiUrl).then(handleResponse);
-    
-    return (null);
-    }
+
+    return null;
+  }
 }
